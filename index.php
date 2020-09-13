afd8443184d5c81e0053a8eb09eab5931c2ea822
Mauricio Nicolás Vega <vegamauricionicolas@gmail.com>
2020-09-13 13:24:09 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container justify-content-center">
+        <div class="row justify-content-center">
+            <h1>Resumen de la prédica del domingo 06-09-2020</h1>
+        </div>
+
+        <br><br><br>
+
+        <div class="row justify-content-center">
+            <h3>Audio en alta calidad</h3>
+        </div>
+        <div class="row justify-content-center">
+            <audio controls>
+                <source src="Resumen Prédica 32.mp3" controls type="audio/mpeg">
+                Your browser does not support the audio tag.
+            </audio>
+        </div>
+
+        <br><br><br>
+
+        <div class="row justify-content-center">
+            <h3>Audio en media calidad</h3>
+        </div>
+        <div class="row justify-content-center">
+            <audio controls>
+                <source src="Resumen Prédica 32.mp3" controls type="audio/mpeg">
+                Your browser does not support the audio tag.
+            </audio>
+        </div>
+
+        <br><br><br>
+
+        <div class="row justify-content-center">
+            <h3>Audio en baja calidad</h3>
+        </div>
+        <div class="row justify-content-center">
+            <audio controls>
+                <source src="Resumen Prédica 32.mp3" controls type="audio/mpeg">
+                Your browser does not support the audio tag.
+            </audio>
+        </div>
+        
+        
+    </div>
+    
+    
+    
+</body>
+</html>
